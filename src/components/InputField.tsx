cd9cf089027b382f7e927b42beb1b2adc0a7c3ac
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-06-26 22:35:31 -0700

    chore: Update npm dependencies

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
 import { Input } from './Input';
 import { Label } from './Label';
- 
 
-interface InputFieldProps {
+interface InputFieldProps<T> {
   id: string;
   label: string;
-  value: number;
-  onChange: (value: number) => void;
+  value: T;
+  onChange: (value: T) => void;
+  type?: string;
 }
 
-const InputField: React.FC<InputFieldProps> = ({ id, label, value, onChange }) => {
+const InputField = <T extends unknown>({ id, label, value, onChange, type = 'number' }: InputFieldProps<T>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = type === 'date' ? new Date(e.target.value) : Number(e.target.value);
+    onChange(val as T);
+  };
+
+  const inputValue = type === 'date' ? (value as Date).toISOString().substr(0, 10) : (value as unknown as string | number);
+
   return (
     <div>
       <Label htmlFor={id}>{label}</Label>
       <Input
         id={id}
-        type="number"
-        value={value}
-        onChange={(e) => onChange(Number(e.target.value))}
+        type={type}
+        value={inputValue}
+        onChange={handleChange}
         className="mt-1 p-2 border rounded bg-gray-700 text-white"
       />
     </div>
