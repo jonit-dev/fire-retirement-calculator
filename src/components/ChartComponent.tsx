7b6ec9157d96dc3d0d0e7b3c12053b26e6457739
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-07-01 14:07:29 -0700

    chore: Disable exhaustive-deps linting rule in ChartComponent and useRetirementCalculator

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Chart, ChartOptions } from 'chart.js';
 import zoomPlugin from 'chartjs-plugin-zoom';
 import { debounce } from 'lodash';
--- src/hooks/useRetirementCalculator.ts
+++ src/hooks/useRetirementCalculator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
 
